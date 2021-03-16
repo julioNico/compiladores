c6ffe5d679bab9205e6de6ec15a48080cac45e99
usiel.lopes <ufljr.1997@gmail.com>
2021-03-16 20:29:38 -0300

    INDENT ORIGINAL - ON

--- trab/2/tests/should_succeed/funcoes.py
+++ trab/2/tests/should_succeed/funcoes.py
@@ -1,15 +1,16 @@
 # abc
 # ABC
-#123
-def funcao(a,b): #dasdasd
+# 123
+def funcao(a, b):  # dasdasd
     # dasdasd
-#dasdasd
+    # dasdasd
     # dadsasd
     a = 10
     return a
 
 # comentario
 
-def funcao2(a,b):
+
+def funcao2(a, b):
     a = 10
     return a
--- trab/2/tests/should_succeed/generico1.py
+++ trab/2/tests/should_succeed/generico1.py
@@ -1,4 +1,4 @@
-a=5
+a = 5
 if a > 1:
-# comentario
-   print(a)
+    # comentario
+    print(a)
